eebeb3a62d14e11cd84cf69540a27fe69b36c406
Jason <iJason92@yahoo.com>
2016-12-15 21:03:04 +0800

    修改README

--- objc4-706/debug-objc/main.m
+++ objc4-706/debug-objc/main.m
@@ -7,11 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "objc.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World!");        
     }
     return 0;
 }
